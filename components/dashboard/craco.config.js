dcc2c06085e0d9db47efe0c305cfa82b6c13afc3
William Ghelfi <trumbitta@gmail.com>
2022-03-28 12:53:35 +0000

    [dashboard] replace dev configuration with env vars

--- components/dashboard/craco.config.js
+++ components/dashboard/craco.config.js
@@ -3,11 +3,27 @@
  * Licensed under the GNU Affero General Public License (AGPL).
  * See License-AGPL.txt in the project root for license information.
  */
+const { whenDev } = require("@craco/craco");
 
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [require("tailwindcss"), require("autoprefixer")],
+    style: {
+        postcss: {
+            plugins: [require("tailwindcss"), require("autoprefixer")],
+        },
     },
-  },
+    ...whenDev(() => ({
+        devServer: {
+            proxy: {
+                "/api": {
+                    target: "https://" + process.env.GP_DEV_HOST,
+                    ws: true,
+                    headers: {
+                        host: process.env.GP_DEV_HOST,
+                        origin: "https://" + process.env.GP_DEV_HOST,
+                        cookie: process.env.GP_DEV_COOKIE,
+                    },
+                },
+            },
+        },
+    })),
 };
