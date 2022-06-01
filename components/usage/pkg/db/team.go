909c41ab2615eb8dd9ec1cb4a33d54f496affabb
Milan Pavlik <milan@gitpod.io>
2022-06-01 07:25:40 +0000

    setup test

--- components/usage/pkg/db/team.go
+++ components/usage/pkg/db/team.go
@@ -9,13 +9,17 @@ import (
 )
 
 type Team struct {
-	ID            string    `gorm:"primary_key;column:id;type:char;size:36;" json:"id"`
-	Name          string    `gorm:"column:name;type:varchar;size:255;" json:"name"`
-	Slug          string    `gorm:"column:slug;type:varchar;size:255;" json:"slug"`
-	CreationTime  string    `gorm:"column:creationTime;type:varchar;size:255;" json:"creation_time"`
-	Deleted       int32     `gorm:"column:deleted;type:tinyint;default:0;" json:"deleted"`
-	LastModified  time.Time `gorm:"column:_lastModified;type:timestamp;default:CURRENT_TIMESTAMP(6);" json:"_last_modified"`
-	MarkedDeleted int32     `gorm:"column:markedDeleted;type:tinyint;default:0;" json:"marked_deleted"`
+	ID   string `gorm:"primary_key;column:id;type:char;size:36;" json:"id"`
+	Name string `gorm:"column:name;type:varchar;size:255;" json:"name"`
+	Slug string `gorm:"column:slug;type:varchar;size:255;" json:"slug"`
+
+	CreationTime VarcharTime `gorm:"column:creationTime;type:varchar;size:255;" json:"creation_time"`
+	LastModified time.Time   `gorm:"column:_lastModified;type:timestamp;default:CURRENT_TIMESTAMP(6);" json:"_last_modified"`
+
+	MarkedDeleted int32 `gorm:"column:markedDeleted;type:tinyint;default:0;" json:"marked_deleted"`
+
+	// deleted is reserved for use by db-sync.
+	_ int32 `gorm:"column:deleted;type:tinyint;default:0;" json:"deleted"`
 }
 
 // TableName sets the insert table name for this struct type
